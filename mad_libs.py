327ce4acfd5e5a8bbf9cebf16295a6fe161e1583
Umidjon Juraboev <tom74459@gmail.com>
2022-10-20 07:21:19 -0700

    add madlibs game.

---
+++ mad_libs.py
@@ -0,0 +1,9 @@
+adj = input('Adjective: ')
+noun = input('Noun: ')
+
+
+# you can add more functions to the game
+madlib = f'''The python language is one of the most {adj} programming languages 
+available because it has simplified {noun} and not complicated, 
+which gives more emphasis on natural language'''
+print(madlib)
